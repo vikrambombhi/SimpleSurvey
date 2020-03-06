3d3cb6411c651617193309294801a495a4d68956
Jacky Chiu <jackyzhanchiu@gmail.com>
2020-03-06 15:12:44 -0500

    add survey tests

--- src/test/java/com/example/survey/SurveyApplicationTests.java
+++ src/test/java/com/example/survey/SurveyApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.survey;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,6 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SurveyApplicationTests {
 	@Autowired
 	private MockMvc mockMvc;
