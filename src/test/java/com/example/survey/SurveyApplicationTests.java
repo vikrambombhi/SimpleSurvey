44abdd2d0c59ff1645be0953cfac5d679cc4a7e4
Jacky Chiu <jackyzhanchiu@gmail.com>
2020-03-06 15:09:00 -0500

    add tests

--- src/test/java/com/example/survey/SurveyApplicationTests.java
+++ src/test/java/com/example/survey/SurveyApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.survey;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class SurveyApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
+	@Test
+	public void newSurvey() throws Exception {
+		this.mockMvc.perform(post("/new")
+				.param("title", "joe")
+		).andExpect(status().isOk());
+
+		this.mockMvc.perform(get("/surveys")
+				.param("id", "1")
+		).andExpect(content().string(containsString("joe")));
+	}
 }
