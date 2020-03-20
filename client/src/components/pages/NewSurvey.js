9dfc954506a3f993d35fd2ef6d0f02e25c282872
Kirin Rastogi <dipshit.dev@gmail.com>
2020-03-19 21:53:50 -0400

    add dropdown to add questions to new survey

--- client/src/components/pages/NewSurvey.js
+++ client/src/components/pages/NewSurvey.js
@@ -8,6 +8,27 @@ export function NewSurvey() {
     <Page
       title="Create a new survey"
       breadcrumbs={[{ content: "Admin Dashboard", url: "/app/admin/surveys" }]}
+      actionGroups={[{
+        title: 'Add Question',
+        accessibilityLabel: 'Add a question',
+        actions: [
+          {
+            content: 'Range Question',
+            accessibilityLabel: 'Add a range question',
+            onAction: () => {},
+          },
+          {
+            content: 'Option Question',
+            accessibilityLabel: 'Add an option question',
+            onAction: () => {},
+          },
+          {
+            content: 'Text Question',
+            accessibilityLabel: 'Add a text question',
+            onAction: () => {},
+          }
+        ],
+      }]}
     >
       <SurveyForm />
     </Page>
