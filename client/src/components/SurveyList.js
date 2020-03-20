77fae17fe1789090bebe610bcf9a5a964548d9d0
Jacky Chiu <jackyzhanchiu@gmail.com>
2020-03-19 21:48:33 -0400

    show close survey if open only, and show users the status

--- client/src/components/SurveyList.js
+++ client/src/components/SurveyList.js
@@ -31,21 +31,27 @@ export function SurveyList({ admin = false }) {
 
   const surveysMarkup =
     surveys && surveys.length > 0
-      ? surveys.map((survey, index) => (
-          <Card
-            key={index}
-            sectioned
-            title={survey.name}
-            actions={
-              admin
-                ? [{ content: "Edit Survey", url: "/app/admin/surveys/edit" },
-                   { content: "Close Survey", onAction: closeSurvey(survey.id) }]
+      ? surveys.map((survey, index) => {
+          let actions = admin
+                ? [{ content: "Edit Survey", url: "/app/admin/surveys/edit" }]
                 : [{ content: "Complete Survey", url: "./todo" }]
-            }
-          >
-            <p>TODO: list questions from this survey here</p>
-          </Card>
-        ))
+
+          if (admin && !survey.closed) {
+            actions.push({ content: "Close Survey", onAction: closeSurvey(survey.id) })
+          }
+
+          return (
+            <Card
+              key={index}
+              sectioned
+              title={survey.name}
+              actions={actions}
+            >
+              <p><b>{survey.closed ? "CLOSED" : "OPEN"}</b></p>
+              <p>TODO: list questions from this survey here</p>
+            </Card>
+          )
+      })
       : null;
 
   return surveysMarkup;
