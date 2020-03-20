b5f3d4179d2df99e1f931d0dd9f787806cea6140
Jacky Chiu <jackyzhanchiu@gmail.com>
2020-03-20 12:32:51 -0400

    add all id to question types

--- src/main/java/com/example/survey/RestDataConfig.java
+++ src/main/java/com/example/survey/RestDataConfig.java
@@ -1,16 +1,15 @@
 package com.example.survey;
 
-import com.example.survey.models.Answer;
-import com.example.survey.models.Question;
-import com.example.survey.models.Survey;
+import com.example.survey.models.*;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.stereotype.Component;
+import org.w3c.dom.ranges.Range;
 
 @Component
 public class RestDataConfig extends RepositoryRestConfigurerAdapter {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        config.exposeIdsFor(Survey.class, Question.class, Answer.class);
+        config.exposeIdsFor(Survey.class, Question.class, RangeQuestion.class, OptionQuestion.class, TextQuestion.class, Answer.class);
     }
 }
