6f736368df3105f6f80871a8a790cdaf35b34611
Jacky Chiu <jackyzhanchiu@gmail.com>
2020-03-19 21:29:48 -0400

    close survey method

--- client/src/components/SurveyList.js
+++ client/src/components/SurveyList.js
@@ -17,6 +17,18 @@ export function SurveyList({ admin = false }) {
     fetchData();
   }, []);
 
+  function closeSurvey(id) {
+    return async function() {
+      const res = await fetch(`/api/close?id=${id}`, {
+        method: "POST"
+      });
+      res
+        .json()
+        .then(res => console.log(res)) // TODO chart answer data
+        .catch(err => console.log(res));
+    }
+  }
+
   const surveysMarkup =
     surveys && surveys.length > 0
       ? surveys.map((survey, index) => (
@@ -26,7 +38,8 @@ export function SurveyList({ admin = false }) {
             title={survey.name}
             actions={
               admin
-                ? [{ content: "Edit Survey", url: "/app/admin/surveys/edit" }]
+                ? [{ content: "Edit Survey", url: "/app/admin/surveys/edit" },
+                   { content: "Close Survey", onAction: closeSurvey(survey.id) }]
                 : [{ content: "Complete Survey", url: "./todo" }]
             }
           >
